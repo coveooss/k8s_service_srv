bd87140583a540173838a87cc4e8905045cb6082
Nicolas Tremblier <ntremblier@coveo.com>
2019-08-30 10:27:00 +0200

    Bug fix

--- k8s_services_srv.py
+++ k8s_services_srv.py
@@ -104,7 +104,7 @@ def update_r53_serviceendpoints(srv_record_name, api_endpoint, k8s_services, all
         all_services = [k8s_services]
     else:
         # If the cluster is not yet in the list of clusters, we add it the the list
-        if next((item for item in all_services if item.keys() == api_endpoint), None) == None:
+        if next((item for item in all_services if api_endpoint in item.keys()), None) == None:
             all_services.append(k8s_services)
 
     # Parsing all clusters found in the TXT record, looking for the current cluster
